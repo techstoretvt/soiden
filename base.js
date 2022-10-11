a7b86e34bff7ba8d6b489acd59d1556c36db72b0
Thoai tran <techstoretvt2565@gmail.com>
2022-10-11 16:28:52 +0700

    commit lần 1

---
+++ base.js
@@ -0,0 +1,11 @@
+
+
+document.addEventListener('mousemove',evt=>{
+    var top = evt.clientY - 120
+    var left = evt.clientX - 120
+
+    document.querySelector('.wrap').style.transform = `translate(${left}px,${top}px)`
+    document.querySelector('.image').style.transform = `translate(-${left}px,-${top}px)`
+
+
+})
